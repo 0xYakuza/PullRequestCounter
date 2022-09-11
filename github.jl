a70b7b26cb5965b1df64624b788f3324bac2dbeb
MahdiA <41341335+AzerothA@users.noreply.github.com>
2022-09-11 10:59:46 +0430

    Add files via upload

--- github.jl
+++ github.jl
@@ -12,33 +12,57 @@ function printTotalPullRequests(result)
     print("---------------------------------------\n")   
 end
 
+function getPullRequests(repo::String, params, auth)
+    requests, page_data = GitHub.pull_requests(repo, params = params, auth = auth)
+    result = Dict{String, Dict}()
+
+    for (index, request) in Iterators.enumerate(requests)
+        ym = Dates.yearmonth(request.created_at)
+
+        if getkey(result, request.user.login, false) !== false && getkey(result[request.user.login], ym, false) !== false
+            result[request.user.login][ym] += 1
+        else
+            get!(result, request.user.login, Dict(ym => 1))
+            get!(result[request.user.login], ym, 1)
+        end
+    end
+
+    return result
+end
+
 DotEnv.config()
 
-configPath = ARGS[1]
+const configPath = ARGS[1]
+const filter = begin 
+    if length(ARGS) == 2
+        ARGS[2]
+    else
+        false
+    end
+end
 
-configFileAsString = open(f->read(f, String), configPath)
+const configFileAsString = open(f->read(f, String), configPath)
+const config = JSON.parse(configFileAsString)
 
-config = JSON.parse(configFileAsString)
-
-myauth = GitHub.authenticate(ENV["GITHUB_AUTH"])
-
-myparams = Dict("state" => "all")
+const auth = GitHub.authenticate(ENV["GITHUB_AUTH"])
 
 for repo in config["repos"]
     print("REPO: ", repo, "\n")
 
-    requests, page_data = GitHub.pull_requests(repo, params = myparams, auth = myauth)
-    result = Dict{String, Dict}()
-
-    for (index, request) in Iterators.enumerate(requests)
-        ymd = Dates.yearmonth(request.created_at)
-        if getkey(result, request.user.login, false) !== false && getkey(result[request.user.login], ymd, false) !== false
-            result[request.user.login][ymd] += 1
-        else
-            get!(result, request.user.login, Dict(ymd => 1))
-            get!(result[request.user.login], ymd, 1)
+    if filter == false
+        params = Dict("state" => "all")
+        getPullRequests(repo, params, auth) |> printTotalPullRequests
+    else
+        params = Dict("state" => "all")
+        requests = getPullRequests(repo, params, auth)
+        datefmt = DateFormat("y-m")
+        dateFromFilter = Date(filter, datefmt)
+        yearAndMonth = Dates.yearmonth(dateFromFilter)
+        
+        print("---------------------------------------\n")   
+        for dev in enumerate(requests)
+            println(dev[2][1], " -> ", get(dev[2][2], yearAndMonth, 0))
         end
+        print("---------------------------------------\n")   
     end
-
-    printTotalPullRequests(result)
 end
